dc5dceeaf30696b68b4ae90d5163bee87285fe46
nurmarief <ariefmuhammad897@gmail.com>
2023-10-17 14:18:21 +0700

    create images dir

---
+++ src/images/index.js
@@ -0,0 +1,7 @@
+import './avatar-michelle.jpg';
+import './drawers.jpg';
+import './favicon-32x32.png';
+import './icon-facebook.svg';
+import './icon-pinterest.svg';
+import './icon-share.svg';
+import './icon-twitter.svg';
